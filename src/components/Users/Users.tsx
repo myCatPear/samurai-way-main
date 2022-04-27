36c6827cdcd9a927df8d7804fa98b3f700b62814
Denis Tavabilov <dagger1233@gmail.com>
2022-04-27 23:19:55 +0500

    end 52lessons

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,43 +5,48 @@ import axios from "axios";
 import avatar from '../../assets/img/ava.png'
 
 export const Users = (props: UsersPropsType) => {
-    if (props.users.length === 0) {
+    const getUser = () => {
+        if (props.users.length === 0) {
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        })
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items)
+            })
 
-        // props.setUsers( [
-        //         {
-        //             id: 1,
-        //             fullName: 'Dmitriy',
-        //             photoUrl: 'https://www.vokrug.tv/pic/person/e/f/7/9/ef793f7a70a9e3f62f6c673cb58cb538.jpg',
-        //             followed: false,
-        //             status: "I am a boss",
-        //             location: {city: "Minsk", country: "Belarus"}
-        //         },
-        //         {
-        //             id: 2,
-        //             fullName: 'Sasha',
-        //             photoUrl: 'https://www.vokrug.tv/pic/person/e/f/7/9/ef793f7a70a9e3f62f6c673cb58cb538.jpg',
-        //             followed: true,
-        //             status: "I am a boss too",
-        //             location: {city: "Moscow", country: "Russia"}
-        //         },
-        //         {
-        //             id: 3,
-        //             fullName: 'Andrew',
-        //             photoUrl: 'https://www.vokrug.tv/pic/person/e/f/7/9/ef793f7a70a9e3f62f6c673cb58cb538.jpg',
-        //             followed: false,
-        //             status: "I am not a boss",
-        //             location: {city: "Kiev", country: "Ukraine"}
-        //         },
-        //     ]
-        // )
+            // props.setUsers( [
+            //         {
+            //             id: 1,
+            //             fullName: 'Dmitriy',
+            //             photoUrl: 'https://www.vokrug.tv/pic/person/e/f/7/9/ef793f7a70a9e3f62f6c673cb58cb538.jpg',
+            //             followed: false,
+            //             status: "I am a boss",
+            //             location: {city: "Minsk", country: "Belarus"}
+            //         },
+            //         {
+            //             id: 2,
+            //             fullName: 'Sasha',
+            //             photoUrl: 'https://www.vokrug.tv/pic/person/e/f/7/9/ef793f7a70a9e3f62f6c673cb58cb538.jpg',
+            //             followed: true,
+            //             status: "I am a boss too",
+            //             location: {city: "Moscow", country: "Russia"}
+            //         },
+            //         {
+            //             id: 3,
+            //             fullName: 'Andrew',
+            //             photoUrl: 'https://www.vokrug.tv/pic/person/e/f/7/9/ef793f7a70a9e3f62f6c673cb58cb538.jpg',
+            //             followed: false,
+            //             status: "I am not a boss",
+            //             location: {city: "Kiev", country: "Ukraine"}
+            //         },
+            //     ]
+            // )
+        }
     }
 
+
     return (
+
         <div>
+            <button onClick={getUser}>get user</button>
             {
                 props.users.map(u => {
                     return <div key={u.id}>
